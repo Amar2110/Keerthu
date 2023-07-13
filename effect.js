5fb95ed425169f69ca5cfb7d2f8bb6eeab8a89cb
Amar2110 <46513566+Amar2110@users.noreply.github.com>
2023-07-13 12:32:49 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -7,16 +7,16 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9,#b10').stop();
-			$('#b11').animate({top:240, left: vw+450},500);
-			$('#b22').animate({top:240, left: vw-350},500);
-			$('#b33').animate({top:240, left: vw-250},500);
-			$('#b44').animate({top:240, left: vw-150},500);
-			$('#b55').animate({top:240, left: vw-50},500);
-			$('#b66').animate({top:240, left: vw+50},500);
-			$('#b77').animate({top:240, left: vw+150},500);
-			$('#b88').animate({top:240, left: vw+250},500);
-			$('#b99').animate({top:240, left: vw+350},500);
-			$('#b1010').animate({top:240, left: vw+450},500);
+			$('#b11').animate({top:240, left: vw+450},800);
+			$('#b22').animate({top:240, left: vw-350},800);
+			$('#b33').animate({top:240, left: vw-250},800);
+			$('#b44').animate({top:240, left: vw-150},800);
+			$('#b55').animate({top:240, left: vw-50},800);
+			$('#b66').animate({top:240, left: vw+50},800);
+			$('#b77').animate({top:240, left: vw+150},800);
+			$('#b88').animate({top:240, left: vw+250},800);
+			$('#b99').animate({top:240, left: vw+350},800);
+			$('#b1010').animate({top:240, left: vw+450},800);
 
 		});
 
